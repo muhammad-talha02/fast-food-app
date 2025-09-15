e6ff7b242cd10afa3573796fd83fdd7859d9df63
M.Talha <iamtalha971@gmail.com>
2025-09-15 22:11:54 +0400

    feat -> SDK version updated to 54

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-100">
-      <Text className="text-xl font-bold text-red-100">
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
         Welcome to Expo!
       </Text>
     </View>
