46e78d285d24585f548008e6605b3752de878a99
M.Talha <iamtalha971@gmail.com>
2025-09-17 22:43:06 +0400

    feat: home page cards done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "./global.css";
+
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
+
+useEffect(() => {
+    if (error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded,error]);
+
+  // if (!fontsLoaded) {
+  //   return null; // or a loading spinner
+  // }
+
+  return <Stack screenOptions={{headerShown:false}}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,58 @@
-import { Text, View } from "react-native";
- 
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Expo!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        contentContainerClassName="pb-28 px-5"
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#ffffff22" }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className="h-full w-1/2">
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "ps-10" : "pe-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor={"#ffffff"}
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
